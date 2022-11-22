298388341b2fd8cb333c9ce5129e6f28f8a53b1d
burnst01 <118362480+burnst01@users.noreply.github.com>
2022-11-22 14:01:15 +0000

    Modals

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+import React, {Component, useState } from 'react';
+import Support from './components/Support';
+import ListCast from './components/ListCast';
+import Modals from './components/Modals';
+
 function App() {
+  const name = 'StarGazers'
+  let [memberInfo, setMemberInfo] =useState(null);
   return (
-    <hgroup>
-      <h1>Meet the StarGazers</h1>
-      <p>Members of an <b>intergalactic alliance</b> paving the way for peace and benevolence among all species. They are known for their enthusiasm for science, for their love of fun, and their dedication to education.</p>
-    </hgroup>
+    <div className="container">
+      <hgroup>
+        <img src="images/group.svg" alt="StarGazers Group" />
+        <h1>Meet the StarGazers</h1>
+        <p>
+          Members of an <b>intergalactic alliance</b> paving the way for peace
+          and benevolence among all species. They are known for their enthusiasm
+          for science, for their love of fun, and their dedication to education.
+        </p>
+        <ListCast
+          onChoice={(info) => {
+            setMemberInfo(info);
+          }}
+        />
+        {memberInfo && <Modals member={memberInfo} handleClose={()=>{setMemberInfo(null)}} />}
+      </hgroup>
+    </div>
   )
 }
 export default App
